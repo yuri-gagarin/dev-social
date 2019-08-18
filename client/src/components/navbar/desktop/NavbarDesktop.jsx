bc5bbad46450a6ae599f28fa6d4c4ab27ccd6f3e
Yuriy Ivanov <ymivanov@icloud.com>
2019-08-18 10:46:51 +0300

    basic layout for sidebars on all devices done

--- client/src/components/navbar/desktop/NavbarDesktop.jsx
+++ client/src/components/navbar/desktop/NavbarDesktop.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import {Container, Icon, Image, Sidebar, Responsive, Menu, Segment} from "semantic-ui-react";
 
 import getInnerSidebarOptions from "../helpers/getInnerSidebarOptions.js";
@@ -25,79 +25,104 @@ const NavbarDesktop = (
   }
 ) => {
   return (
-    <Sidebar.Pushable>
-      <Sidebar
-        as={Menu}
-        animation="overlay"
-        vertical
-        direction="top"
-        visible={innerMainVisible}
-        id={style.mainMenu}>
-        <Menu.Item
-          as={"a"}
-          onClick={onInnerMainToggle}
-          className={""} >
-          <div><Icon name={"arrow left"}></Icon></div>
-          <div>Back</div>
+    <div>
+      <Menu id={style.navbarDesktop} fixed="top">
+        <Menu.Item>
+          <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
         </Menu.Item>
-        { 
-          getInnerSidebarOptions(innerMainItems, innerMainToOpen).map((item) => {
-            return (
-              <Menu.Item
-                {...item}
-                className={""}>
-              </Menu.Item>
-            );
+        <Menu.Item onClick={onMainToggle}>
+          <Icon name="sliders horizontal"></Icon>
+          <span id={style.mainMenuBtn}>Main Menu</span>
+        </Menu.Item>
+        <Menu.Menu position="right">
+        { rightItems.map((item) => {
+            return( 
+            <Menu.Item {...item} onClick={onRightToggle}
+              data-inner={item.content}>
+            </Menu.Item>
+            )
           })
         }
-      </Sidebar>
-      
-      <Sidebar
-        as={Container}
-        animation="overlay"
-        visible={rightVisible}
-        direction="top"
-        style={{width: "100vw", minHeight: "100vh", backgroundColor: "white"}} >
-        <Menu.Item
-          as={Segment}
-          onClick={onRightToggle} >
-          <Icon name="window close outline"></Icon>
-          <div>Close</div>
-        </Menu.Item>
-        { buildRightMenu(rightInnerItems, {}) }
-      </Sidebar>
-      <Sidebar.Pusher
-        dimmed={pusherVisible}
-        onClick={onPusherToggle}
-        style={ {height: "100vh"} }>
-        <Menu fixed="top" id={style.navbarDesktop}>
-          <Menu.Item>
-            <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+        </Menu.Menu>
+      </Menu>
+      <Sidebar.Pushable id={style.sidebarPushable}>
+        
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          visible={innerMainVisible}
+          direction="left"
+          vertical
+          id={style.innerMainMenu}>
+          <Menu.Item
+            as={Segment}
+            onClick={onInnerMainToggle}
+            className={""} >
+            <div><Icon name={"arrow left"}></Icon></div>
+            <div>Back</div>
           </Menu.Item>
-          { mainItems.map((item) => {
+          { 
+            getInnerSidebarOptions(innerMainItems, innerMainToOpen).map((item) => {
               return (
-                <Menu.Item {...item} 
-                  className={style.mainItem}
+                <Menu.Item
+                  {...item}
+                  className={""}>
+                </Menu.Item>
+              );
+            })
+          }
+        </Sidebar>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          visible={mainVisible}
+          direction="left"
+          vertical
+          id={style.mainMenu}>
+          <Menu.Item
+            as={Segment}
+            onClick={onMainToggle}
+            className={""} >
+            <div><Icon name={"arrow left"}></Icon></div>
+            <div>Back</div>
+          </Menu.Item>
+          {
+            mainItems.map((item) => {
+              return (
+                <Menu.Item
+                {...item}
                   onClick={onInnerMainToggle}
                   data-inner={item.content}>
                 </Menu.Item>
               );
             })
           }
-          <Menu.Menu position="right">
-          { rightItems.map((item) => {
-              return( 
-              <Menu.Item {...item} onClick={onRightToggle}
-                data-inner={item.content}>
-              </Menu.Item>
-              )
-            })
-          }
-          </Menu.Menu>
-        </Menu>
-        {children}
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+
+        </Sidebar>
+        <Sidebar
+          as={Container}
+          animation="overlay"
+          visible={rightVisible}
+          direction="top"
+          style={{width: "100vw", minHeight: "100vh", backgroundColor: "white"}} >
+          <Menu.Item
+            as={Segment}
+            onClick={onRightToggle} >
+            <Icon name="window close outline"></Icon>
+            <div>Close</div>
+          </Menu.Item>
+          { buildRightMenu(rightInnerItems, {}) }
+        </Sidebar>
+        <Sidebar.Pusher
+          dimmed={pusherVisible}
+          onClick={onPusherToggle}
+          style={ {height: "100vh"} }>
+          
+          {children}
+        </Sidebar.Pusher>
+        
+      </Sidebar.Pushable>
+    </div>
   );
 };
 
