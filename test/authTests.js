bcb7951f29e2a90a4fa614c5989109095406dfea
Yuriy Ivanov <ymivanov@icloud.com>
2019-09-19 02:26:30 +0300

    Quick cleanup of Auth tests

--- test/authTests.js
+++ test/authTests.js
@@ -1,57 +1,62 @@
 import chai, {expect} from "chai";
 import chaiHttp from "chai-http";
 import app from "../server.js";
+import mongoose from "mongoose";
+import faker from "faker";
+import keys from "../config/keys.js";
 import User from "../models/User.js";
 //configure
 chai.use(chaiHttp);
 chai.should();
+const mongoURI = keys.mongoURI;
 
-describe("Authorization Tests", function() {
+describe("Auth Tests", function() {
 
-  after("Exit the function", function() {
-    User.deleteMany({}, (err) => {
-      if(err) {
-        console.error(err)
-        process.exit(1);
-      }
-      process.exit(0);
-    });
+  const validUser = {
+    name: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: "Password1",
+    passwordConfirm: "Password1",
+  };
+  before("Connect to Database", function(done) {
+    mongoose.connect(mongoURI, {useNewUrlParser: true})
+      .then(() =>  done())
+      .catch((error) => console.error(error));
+  })
+  before("Clear Databse", function(done) {
+    mongoose.connection.db.dropDatabase()
+      .then(() => done())
+      .catch((error) => console.error(error));
   });
-
+  after("Clean Up", function(done) {
+    mongoose.connection.db.dropDatabase()
+      .then(() => done())
+      .catch((error) => console.error(error));
+  });
+  
   describe("User Registration", function() {
-    const validUser = {
-      name: "User",
-      lastName: "User",
-      email: "user@user.com",
-      password: "Password1",
-      passwordConfirm: "Password1",
-    };
-
-    before("Clear any User data", function(done) {
-      User.deleteMany({}, (err) => {
-        if(err) {
-          console.error("error");
-        }
-        done();
-      });
-    });
-    describe("POST /api/register \n", function() {
+    
+    describe("POST /api/register ", function() {
       it("Should register a user", function(done) {
         chai.request(app)
           .post("/api/users/register")
           .send(validUser)
           .end((error, response) => {
+            expect(error).to.be.null;
+            expect(response).to.have.status(200);
             done();
           });
       });
-
       it("Should not register an invalid user", function(done) {
         const invalidUser = {name: null, firstName: null,  email: null, password: null};
         chai.request(app)
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response) => {
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
+            expect(response.body.errors).to.not.be.null;
             done();
           });
       });
@@ -61,10 +66,10 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response) => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.name).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.name).to.not.be.null;
             done();
           });
       });
@@ -75,10 +80,10 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response)  => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.name).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.name).to.not.be.null;
             done();
           });
       });
@@ -89,10 +94,10 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response) => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.email).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.email).to.not.be.null;
             done();
           });
       });
@@ -103,10 +108,10 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response) => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.password).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.password).to.not.be.null;
             done();
           });
       });
@@ -117,10 +122,10 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(invalidUser)
           .end((error, response) => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.password).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.password).to.not.be.null;
             done();
           });
       });
@@ -130,53 +135,34 @@ describe("Authorization Tests", function() {
           .post("/api/users/register")
           .send(validUser)
           .end((error, response) => {
-            const responseText = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(responseText.errors).to.not.be.null;
-            expect(responseText.errors.message).to.not.be.null;
+            expect(response.body.errors).to.not.be.null;
+            expect(response.body.errors.message).to.not.be.null;
             done();
           });
       });
-    });
-  });
-
-
-  describe("User Login", function() {
-    const validUser = {
-      name: "User",
-      lastName: "User",
-      email: "user@user.com",
-      password: "Password1",
-      passwordConfirm: "Password1",
-    };
-    before("Clear Database", function(done) {
-      User.deleteMany({}, (err) => {
-        if (err) {console.log(err)};
-        done();
+      it("should only have one User in Database", function(done) {
+        User.countDocuments({}, (err, count) => {
+          expect(count).to.equal(1);
+          done();
+        });
       });
     });
-    before("Register a valid User", function(done) {
-      chai.request(app)
-        .post("/api/users/register")
-        .send(validUser)
-        .end((error, response) => {
-          if(error) {console.log(error)};
-          if(response.status == 200) {
-            done();
-          }
-        });
-    });
+  });
+  // end User Registration //
+  describe("User Login", function() {
+
     describe("POST /api/login", function() {
       it("Should Login a user", function(done) {
         chai.request(app)
           .post("/api/users/login")
           .send({email: validUser.email, password: validUser.password})
           .end((error, response) => {
-            const userResponse = JSON.parse(response.text);
             expect(error).to.be.null;
             expect(response).to.have.status(200);
-            expect(userResponse.token).to.not.be.null;
-            expect(userResponse.user).to.not.be.null;
+            expect(response.body.token).to.not.be.null;
+            expect(response.body.user).to.not.be.null;
             done();
           });
       });
@@ -185,9 +171,9 @@ describe("Authorization Tests", function() {
           .post("/api/users/login")
           .send({email: null, password: null})
           .end((error, response) => {
-            const userResponse = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(response.message).to.not.be.null;
+            expect(response.body.meessage).to.not.be.null;
             done();
           });
       });
@@ -196,9 +182,9 @@ describe("Authorization Tests", function() {
           .post("/api/users/login")
           .send({email: "none@mail.com", password: "Password1"})
           .end((error, response) => {
-            const userResponse = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(response.message).to.not.be.null;
+            expect(response.body.message).to.not.be.null;
             done();
           });
       });
@@ -207,14 +193,15 @@ describe("Authorization Tests", function() {
           .post("/api/users/login")
           .send({email: validUser.email, password: "Incorrect1"})
           .end((error, response) => {
-            const userResponse = JSON.parse(response.text);
+            expect(error).to.be.null;
             expect(response).to.have.status(400);
-            expect(response.message).to.not.be.null;
+            expect(response.body.message).to.not.be.null;
             done();
           });
       });
     });
   });
+  // end User Login //
 });
 
 
