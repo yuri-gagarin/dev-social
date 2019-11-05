5ef978e5b37af0f522f13da7ab6353b743fc8bcb
Yuriy Ivanov <ymivanov@icloud.com>
2019-11-05 23:58:12 +0200

    component test setup with enzyme

--- client/test/posts/PostsNavbar.spec.js
+++ client/test/posts/PostsNavbar.spec.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PostsNavbar from "../../src/components/posts/post_navbar/PostsNavbar";
 import renderer from "react-test-renderer";
+import { shallow, mount } from "enzyme";
+import { Menu, Dropdown } from "semantic-ui-react";
 
 describe("PostNavbar test", function() {
   const component = renderer.create(<PostsNavbar/>);
@@ -8,4 +10,21 @@ describe("PostNavbar test", function() {
   test("Component renders", () => {
     expect(tree).toMatchSnapshot();
   });
+  it("renders the component", () => {
+    const wrapper = shallow(<PostsNavbar />);
+  });
+  it("has a Menu component", () => {
+    const wrapper = mount(<PostsNavbar />);
+    console.log(wrapper.find(Menu).length);
+    expect(wrapper.find(Menu).length).toEqual(1)
+  });
+  it("has two Dropdown components", () => {
+    const wrapper = mount(<PostsNavbar />);
+    expect(wrapper.find(Dropdown).length).toEqual(2);
+  });
+  it("sets the default state", () => {
+    const wrapper = mount(<PostsNavbar />);
+    expect(wrapper.state("filter")).toEqual("new");
+  });
+
 })
