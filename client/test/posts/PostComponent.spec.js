83c90d04ddf190b46838901673f2dcf11d50b1e9
Yuriy Ivanov <ymivanov@icloud.com>
2019-11-13 23:54:57 +0200

    PostComponent tests as i like them at this point

--- client/test/posts/PostComponent.spec.js
+++ client/test/posts/PostComponent.spec.js
@@ -63,40 +63,51 @@ describe("PostComponent tests", () => {
     });
   });
   describe("The component should correctly process dates", () => {
-    it("Should correctly display createdAt date", () => {
+    describe("Componet gets passed a valid date string", () => {
       const wrapper = shallow(<PostComponent post={post} toggleLike={likeMock} toggleDislike={dislikeMock} />);
-      const postCreatedAt = formatDate(post.createdAt, {military: false});
-      const createdDate = wrapper.find(`[data-test="post-created-date"]`);
-      expect(createdDate.props().children[1]).toEqual(postCreatedAt);
+      it("Should correctly display createdAt date", () => {
+        const postCreatedAt = formatDate(post.createdAt, {military: false});
+        const createdDate = wrapper.find(`[data-test="post-created-date"]`);
+        expect(createdDate.props().children[1]).toEqual(postCreatedAt);
+      });
+      it("should correctly display the editedAt date", () => {
+        const postEditedAt = formatDate(post.editedAt, {military: false});
+        const editedDate = wrapper.find(`[data-test="post-edited-date"]`);
+        expect(editedDate.props().children[1]).toEqual(postEditedAt);
+      });
     });
-    it("should correctly display the editedAt date", () => {
-      const wrapper= shallow(<PostComponent post={post}toggleLike={likeMock} toggleDislike={dislikeMock}  />);
-      const postEditedAt = formatDate(post.editedAt, {military: false});
-      const editedDate = wrapper.find(`[data-test="post-edited-date"]`);
-      expect(editedDate.props().children[1]).toEqual(postEditedAt);
-    });
-    it("Should display an empty string if no createdAt date", () => {
-      const wrapper = shallow(<PostComponent post={{...post, createdAt: null}} toggleLike={likeMock} toggleDislike={dislikeMock}/>);
-      const postCreatedAt = formatDate(null, {military: false});
-      const createdDate = wrapper.find(`[data-test="post-created-date"]`);
-      expect(createdDate.props().children[1]).toEqual(postCreatedAt);
-    });
-    it("Should display an empty string if no editedAt date", () => {
-      const wrapper = shallow(<PostComponent post={{...post, editedAt: null}} toggleLike={likeMock} toggleDislike={dislikeMock} />);
-      const postEditedAt = formatDate(null, {military: false});
-      const editedDate = wrapper.find(`[data-test="post-edited-date"]`);
-      expect(editedDate.props().children[1]).toEqual(postEditedAt);
+    describe("Component gets passed a null or undefined date", () => {
+      const wrapper = shallow(<PostComponent post={{...post, createdAt: null, editedAt: null}} 
+                              toggleLike={likeMock} toggleDislike={dislikeMock} />);
+      it("Should display an empty string if no createdAt date", () => {
+        const postCreatedAt = formatDate(null, {military: false});
+        const createdDate = wrapper.find(`[data-test="post-created-date"]`);
+        expect(createdDate.props().children[1]).toEqual(postCreatedAt);
+      });
+      it("Should display an empty string if no editedAt date", () => {
+        const postEditedAt = formatDate(null, {military: false});
+        const editedDate = wrapper.find(`[data-test="post-edited-date"]`);
+        expect(editedDate.props().children[1]).toEqual(postEditedAt);
+      });
     });
   });
-  describe("It should correctly display likes and dislikes", () => {
+  describe("The component should correctly display likes and dislikes", () => {
+    const wrapper = shallow(<PostComponent post={post} toggleLike={likeMock} toggleDislike={dislikeMock} />);
+    it("Should display the correct like count", () => {
+      const likeCount = wrapper.find(`[data-test="like-count"]`);
+      expect(likeCount.props().children[1]).toEqual(post.likeCount);
+    });
+    it("Should display the correct dislike count", () => {
+      const dislikeCount = wrapper.find(`[data-test="dislike-count"]`);
+      expect(dislikeCount.props().children[1]).toEqual(post.dislikeCount);
+    });  
+  });
 
-  })
-  describe("It should correctly update likePost, dislikePost", () => {
-    
+  describe("The component should correctly update likePost, dislikePost", () => {
+    const wrapper = mount(<PostComponent post={post} toggleLike={likeMock} toggleDislike={dislikeMock} />);
+
     it("Should like the post and update", () => {
-      const wrapper = mount(<PostComponent post={post} toggleLike={likeMock} toggleDislike={dislikeMock} />);
       const likePostBtn = wrapper.find(`[data-test="like-post-toggle"]`);
-
       let likeCount = wrapper.find(`[data-test="like-count"]`);
       expect(likeCount.props().children[1]).toEqual(post.likeCount);
       //click like post
@@ -105,7 +116,18 @@ describe("PostComponent tests", () => {
       wrapper.setProps({post: updatedPost});
       likeCount = wrapper.find(`[data-test="like-count"]`);
       expect(likeCount.props().children[1]).toEqual(updatedPost.likeCount);
+    });
+    it("Should dislike the post and update", () => {
+      const dislikePostBtn = wrapper.find(`[data-test="dislike-post-toggle"]`);
+      let dislikeCount = wrapper.find(`[data-test="dislike-count"]`);
+      expect(dislikeCount.props().children[1]).toEqual(post.dislikeCount);
+      //click dislike on the post
+      dislikePostBtn.simulate("click");
+      const updatedPost = dislikeMock(post);
+      wrapper.setProps({post: updatedPost});
+      dislikeCount = wrapper.find(`[data-test="dislike-count"]`);
+      expect(dislikeCount.props().children[1]).toEqual(updatedPost.dislikeCount);
+    });
+  });
 
-    })
-  })
 })
