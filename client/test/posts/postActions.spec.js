9fd372a7139d0ff29f17da895cf99bd4658dda1b
Yuriy Ivanov <ymivanov@icloud.com>
2019-12-05 18:35:56 +0200

    passing tests for PostLike, PostDislike actions. Maybe some comments lateR

--- client/test/posts/postActions.spec.js
+++ client/test/posts/postActions.spec.js
@@ -80,19 +80,23 @@ describe("Post Actions Tests", () => {
   
   // 
   describe("Post {Like} {Dislike} toggles tests", () => {
-    beforeAll(() => {
+    beforeEach(() => {
       //localStorage mock token
       localStorage.setItem(JWT_TOKEN,  "a_fake_token")
     })
     describe(`Action: {likePost}`, () => {
       it(`Should dispatch a ${types.LIKE_POST} action`, () => {
         // a liked post for mock API call return
-        const updatedPost = {...posts[0]};
-        updatedPost.likeCount +=1;
-        updatedPost.markLiked = true;
-        // the shape of new postState with the first post liked.
-        const newPostsState = [...posts];
-        newPostsState[0] = {...updatedPost};
+        const currentPostsState = posts.slice();
+        //expected updated post //
+        const updatedPost = {
+          ...currentPostsState[0],
+          markLiked: true,
+          likeCount: currentPostsState[0].likeCount += 1,
+        };
+        //expected postsState to be passed to reducer//
+        const newPostsState = currentPostsState.slice(1);
+        newPostsState.unshift(updatedPost);
   
         moxios.wait(() => {
           let request = moxios.requests.mostRecent();
@@ -108,14 +112,12 @@ describe("Post Actions Tests", () => {
           { type: types.LIKE_POST, payload: {message: "liked", posts: newPostsState} }
         ];
         const postId = posts[0]._id;
-        return testStore.dispatch(likePost(postId, posts))
+        return testStore.dispatch(likePost(postId, currentPostsState))
           .then(() => {
-            //console.log(testStore.getActions())
-            //console.log(expectedActions)
             expect(testStore.getActions()).toEqual(expectedActions);
           });
       });
-      it(`Should successfully handle an error`, () => {
+      it(`Should successfully handle an API error`, () => {
         const error = new Error("Oops");
         moxios.wait(() => {
           let request = moxios.requests.mostRecent();
@@ -131,22 +133,37 @@ describe("Post Actions Tests", () => {
           .then(() => {
             expect(testStore.getActions()).toEqual(expectedActions);
           });
-      })
+      });
+      it("Should throw an error if no token is in {localStorage}", () => {
+        //remove jwtToken to simulate a user not logged in
+        localStorage.removeItem(JWT_TOKEN);
+        const expectedActions = [
+          { type: types.POSTS_ERROR, payload: {message: loginError.message, error: loginError, loading: false} }
+        ];
+
+        return testStore.dispatch(likePost(123, posts))
+          .then(() => {
+            expect(testStore.getActions()).toEqual(expectedActions);
+          });
+      });
     })
     describe("Action: {removePostLike}", () => {
       it(`Should successfully dispatch a ${types.REMOVE_POST_LIKE} action`, () => {
         //in the mock state the post is liked
-        const likedPost = {...posts[0]};
-        likedPost.likeCount = 1;
-        likedPost.markLiked = true;
-        const currentPostState = posts.slice(1)
-        currentPostState.unshift(likedPost);
-        //expected that all posts will have no user like
-        const updatedPost = {...posts[0]}
-        updatedPost.likeCount -= 1;
-        updatedPost.markLiked = false;
-        const expectedPostState = posts.slice(1);
-        expectedPostState.unshift(updatedPost);
+        const currentPostsState = posts.slice()
+        //expected updated post //
+        currentPostsState[0].likeCount = 1;
+        currentPostsState[0].markLiked = true;
+        const postId = currentPostsState[0]._id;
+        //expected updated post //
+        const updatedPost = {
+          ...currentPostsState[0], 
+          markLiked: false,
+          likeCount: currentPostsState[0].likeCount - 1,
+        };
+        //expected postsState to be passed to reducer//
+        const newPostsState = posts.slice(1)
+        newPostsState.unshift(updatedPost);
   
         moxios.wait(() => {
           let request = moxios.requests.mostRecent();
@@ -160,15 +177,14 @@ describe("Post Actions Tests", () => {
         });
   
         const expectedActions = [
-          { type: types.REMOVE_POST_LIKE, payload: {message: "unlike post", posts: expectedPostState} }
+          { type: types.REMOVE_POST_LIKE, payload: {message: "unlike post", posts: newPostsState} }
         ];
-        const postId = updatedPost._id;
-        return testStore.dispatch(removePostLike(postId, currentPostState))
+        return testStore.dispatch(removePostLike(postId, currentPostsState))
           .then(() => {
             expect(testStore.getActions()).toEqual(expectedActions);
           });
       });
-      it(`Should successfully handle an error`, () => {
+      it(`Should successfully handle an API error`, () => {
         const error = new Error("Oops");
         moxios.wait(() => {
           let request = moxios.requests.mostRecent();
@@ -184,17 +200,38 @@ describe("Post Actions Tests", () => {
           .then(() => {
             expect(testStore.getActions()).toEqual(expectedActions);
           });
-      })
+      });
+      it("Should throw an error if no token is in {localStorage}", () => {
+        //remove jwtToken to simulate a user not logged in
+        localStorage.removeItem(JWT_TOKEN);
+        const expectedActions = [
+          { type: types.POSTS_ERROR, payload: {message: loginError.message, error: loginError, loading: false} }
+        ];
+
+        return testStore.dispatch(removePostLike(123, posts))
+          .then(() => {
+            expect(testStore.getActions()).toEqual(expectedActions);
+          });
+      });
+
     });
     describe("Action: {dislikePost}", () => {
       it(`Should successfully dispatch a ${types.DISLIKE_POST} action`, () => {
-        const updatedPost = {...posts[1]};
+        //current postsState //
+        const currentPostsState = posts.slice();
+        //expected updated post
+        const updatedPost = {
+          ...currentPostsState[0],
+          markDisliked: true,
+          dislikeCount: currentPostsState[0].dislikeCount += 1,
+        };
         updatedPost.dislikeCount +=1;
         updatedPost.markDisliked = true;
-        //
-        const newPostsState = [...posts];
-        newPostsState[1] = {...updatedPost};
-        //console.log(newPostsState)
+        const postId = currentPostsState[0]._id;
+        //expected postsState //
+        const newPostsState = currentPostsState.slice(1);
+        newPostsState.unshift(updatedPost);
+
         moxios.wait(() => {
           let request = moxios.requests.mostRecent();
           request.respondWith({
@@ -205,7 +242,6 @@ describe("Post Actions Tests", () => {
             }
           })
         });
-        const postId = updatedPost._id;
         const expectedActions = [
           { type: types.DISLIKE_POST, payload: {message: "disliked post", posts: newPostsState} }
         ];
@@ -230,7 +266,19 @@ describe("Post Actions Tests", () => {
           .then(() => {
             expect(testStore.getActions()).toEqual(expectedActions);
           });
-      })
+      });
+      it("Should throw an error if no token is in {localStorage}", () => {
+        //remove jwtToken to simulate a user not logged in
+        localStorage.removeItem(JWT_TOKEN);
+        const expectedActions = [
+          { type: types.POSTS_ERROR, payload: {message: loginError.message, error: loginError, loading: false} }
+        ];
+
+        return testStore.dispatch(dislikePost(123, posts))
+          .then(() => {
+            expect(testStore.getActions()).toEqual(expectedActions);
+          });
+      });
     });
     
     describe("Action: {removePostDislike}", () => {
@@ -286,17 +334,19 @@ describe("Post Actions Tests", () => {
             expect(testStore.getActions()).toEqual(expectedActions);
           });
       });
+      
       it("Should throw an error if no token is in {localStorage}", () => {
-        localStorage.setItem(JWT_TOKEN, null);
+        //remove jwtToken to simulate a user not logged in
+        localStorage.removeItem(JWT_TOKEN);
         const expectedActions = [
-          { type: types.POSTS_ERROR, payload: {message: loginError.message, error: loginError} }
+          { type: types.POSTS_ERROR, payload: {message: loginError.message, error: loginError, loading: false} }
         ];
 
         return testStore.dispatch(removePostDislike(123, posts))
           .then(() => {
             expect(testStore.getActions()).toEqual(expectedActions);
-          })
-      })
+          });
+      });
     });
     
   })
