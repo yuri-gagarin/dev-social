770ff77efa9e361a480f31932b5f6853234da35e
Yuriy Ivanov <ymivanov@icloud.com>
2019-12-11 20:15:02 +0200

    add a {commentsReducer} - not tested

---
+++ client/src/redux/reducers/commentsReducer.js
@@ -0,0 +1,72 @@
+import { FETCH_COMMENTS, COMMENTS_REQUEST, COMMENTS_SUCCESS, COMMENTS_ERROR, 
+         LIKE_COMMENT, REMOVE_COMMMENT_LIKE, DISLIKE_COMMENT, REMOVE_COMMENT_DISLIKE, COMMENTS_ERROR } from "../cases";
+
+const initialState = {
+  message: "",
+  loading: false,
+  comments: [],
+  commentsError: null
+};
+
+const commentsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case COMMENTS_REQUEST: 
+      return {
+        ...state,
+        loading: true,
+        commentsError: null,
+      };
+    case COMMENTS_SUCCESS: 
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        comments: [...action.payload.comments],
+        commentsError: null,
+      };
+    case LIKE_COMMENT: 
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        comments: [...action.payload.comments],
+        commentsError: null,
+      };
+    case REMOVE_COMMMENT_LIKE:
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        comments: [...action.payload.comments],
+        commentsError: null,
+      };
+    case DISLIKE_COMMENT:
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        comments: [...action.payload.comments],
+        commentsError: null,
+      };
+    case REMOVE_COMMENT_DISLIKE: 
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        comments: [...action.payload.comments],
+        commentsError: null,
+      }; 
+    case COMMENTS_ERROR: 
+      return {
+        ...state,
+        loading: false,
+        message: action.payload.message,
+        commentsError: {...action.payload.error}
+      };
+    default: {
+      return state;
+    };
+  };
+};
+
+export default commentsReducer;
--- client/src/redux/reducers/indexReducer.js
+++ client/src/redux/reducers/indexReducer.js
@@ -1,12 +1,14 @@
 import {combineReducers} from "redux";
-import authReducer from "./authReducer.js";
+import authReducer from "./authReducer";
+import commentsReducer from "./commentsReducer";
 import {navReducer} from "./uiReducers";
-import postReducer from "./postReducer.js";
-import testReducer from "./testReducer.js";
-import errorReducer from "./errorReducer.js";
+import postReducer from "./postReducer";
+import testReducer from "./testReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
   authState: authReducer,
+  commentsState: commentsReducer,
   navState: navReducer,
   postsState: postReducer,
   test: testReducer,
