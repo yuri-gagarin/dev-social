0b6a0b88593bccbe7dcfa3d3854cce83a8eb78cf
Yuriy Ivanov <ymivanov@icloud.com>
2019-12-18 23:58:07 +0200

    adding end to end integration tests on some redux actions

---
+++ client/test/posts/integration/postActionIntegration.spec.js
@@ -0,0 +1,33 @@
+// integration redux action tests with real server response //
+
+describe("postActions integration tests", () => {
+  // User is Logged IN //
+  describe("User IS logged in", () => {
+    // login user - get JWT token //
+    beforeAll(() => {
+
+    });
+    // {createPost} Redux and API action //
+    describe("Action {createPost}",  () => {
+      it("Should create a Post, save to database and updated state", () => {
+      
+      });
+    });
+    // END {createPost} Redux and API action //
+    // {saveEditedPost} Redux and API action //
+    describe("Action {savedEditedPost}", () => {
+      it("Should save an edited Post and update state", () => {
+
+      });
+    });
+    // END {saveEditedPost} Redux and API action //
+    // {deletePost} Redux and API action //
+    describe("Action {deletePost}", () => {
+      it("Should delete a specific post and update state", () => {
+
+      });
+    });
+    // END {deletePost} Redux and API action //
+  });
+  // END user is logged in 
+})
